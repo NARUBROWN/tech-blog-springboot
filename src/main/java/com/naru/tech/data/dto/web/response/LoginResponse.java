098ebfc9736424d4529a9100b8a45f34b0b9e45f
NARUBROWN <codingshark@inu.ac.kr>
2025-12-18 10:57:26 +0900

    [feat] Include user ID in LoginResponse DTO

--- src/main/java/com/naru/tech/data/dto/web/response/LoginResponse.java
+++ src/main/java/com/naru/tech/data/dto/web/response/LoginResponse.java
@@ -3,12 +3,13 @@ package com.naru.tech.data.dto.web.response;
 import com.naru.tech.data.domain.User;
 
 public record LoginResponse(
+        Long id,
         String username,
         String email,
         String profileImageUrl,
         String role
 ) {
     public static LoginResponse fromEntity(User user) {
-        return new LoginResponse(user.getUsername(), user.getEmail(), user.getProfileImageUrl(), user.getRole().name());
+        return new LoginResponse(user.getId(), user.getUsername(), user.getEmail(), user.getProfileImageUrl(), user.getRole().name());
     }
 }
